430c0148dedcbcf4532526196eb2af412c18d54d
Rupin Munjal <rupinmunjal05@gmail.com>
2024-10-20 18:41:21 -0400

    Add color changer project

---
+++ 1-colorChanger/chaiaurcode.js
@@ -0,0 +1,8 @@
+const buttons = document.querySelectorAll(".button")
+const body = document.querySelector("body")
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        body.style.backgroundColor = button.id
+    })
+})
